c983ab602c15113456bfe3a5391eb668a453be96
stockcode <stockcode@sina.cn>
2014-07-08 17:27:13 +0800

    combo

--- autotrade/Form1.cs
+++ autotrade/Form1.cs
@@ -7,16 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using autotrade.strategy;
 using CTPMdApi;
 using CTPTradeApi;
+using MongoDB.Driver.Linq;
+using CThostFtdcReqQueryAccountField = CTPTradeApi.CThostFtdcReqQueryAccountField;
+using CThostFtdcRspInfoField = CTPTradeApi.CThostFtdcRspInfoField;
+using CThostFtdcTradingAccountField = CTPTradeApi.CThostFtdcTradingAccountField;
 
 namespace autotrade
 {
     public partial class Form1 : Form
     {
         private readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        MdApi mdApi = new MdApi("gengke", "123");
-        TradeApi tradeApi = new TradeApi("gengke", "123");
+        MdApi mdApi = new MdApi("50001693", "05221058", "8888", "tcp://210.51.25.90:32111");
+        TradeApi tradeApi = new TradeApi("50001693", "05221058", "8888", "tcp://210.51.25.90:41205");
+        private MAReverseStrategy maReverseStrategy;
 
         readonly string[] ppInstrumentID = { "IF1407" };	// 行情订阅列表
         public Form1()
@@ -26,6 +32,8 @@ namespace autotrade
 
         private void button1_Click(object sender, EventArgs e)
         {
+            maReverseStrategy = new MAReverseStrategy(20, "IF1407");
+
             log.Info("start login");
 
             mdApi.Connect();
@@ -41,6 +49,7 @@ namespace autotrade
         void mdApi_OnRtnDepthMarketData(ref CTPMdApi.CThostFtdcDepthMarketDataField pDepthMarketData)
         {
             string s = string.Format("{0,-6} : UpdateTime = {1}.{2:D3},  LasPrice = {3}", pDepthMarketData.InstrumentID, pDepthMarketData.UpdateTime, pDepthMarketData.UpdateMillisec, pDepthMarketData.LastPrice);
+            s += maReverseStrategy.Match(pDepthMarketData);
             log.Info(s);
         }
 
@@ -51,7 +60,8 @@ namespace autotrade
 
         void mdApi_OnRspUserLogin(ref CTPMdApi.CThostFtdcRspUserLoginField pRspUserLogin, ref CTPMdApi.CThostFtdcRspInfoField pRspInfo, int nRequestId, bool bIsLast)
         {
-            log.Info(pRspInfo.ErrorMsg);
+            log.Info(pRspInfo);
+            log.Info(pRspUserLogin);
             mdApi.SubMarketData(ppInstrumentID);
         }
 
@@ -72,11 +82,43 @@ namespace autotrade
             tradeApi.Connect();
 
             tradeApi.OnFrontConnect += tradeApi_OnFrontConnect;
+            tradeApi.OnRspUserLogin += tradeApi_OnRspUserLogin;
+            tradeApi.OnRspQryTradingAccount += tradeApi_OnRspQryTradingAccount;
+            tradeApi.OnRspQryInvestorPosition += tradeApi_OnRspQryInvestorPosition;
+
+        }
+
+        void tradeApi_OnRspQryInvestorPosition(ref CTPTradeApi.CThostFtdcInvestorPositionField pInvestorPosition, ref CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast)
+        {
+            log.Info(pRspInfo);
+            log.Info(pInvestorPosition);
+        }
+
+        void tradeApi_OnRspQryTradingAccount(ref CThostFtdcTradingAccountField pTradingAccount, ref CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast)
+        {
+            log.Info(pRspInfo.ErrorMsg);
+            log.Info(pTradingAccount);
+        }
+
+        void tradeApi_OnRspUserLogin(ref CTPTradeApi.CThostFtdcRspUserLoginField pRspUserLogin, ref CTPTradeApi.CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast)
+        {
+            log.Info(pRspInfo);
+            log.Info(pRspUserLogin);
+
+
+            tradeApi.QryTradingAccount();
         }
 
         void tradeApi_OnFrontConnect()
         {
+            log.Info("connected");
+
             tradeApi.UserLogin();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tradeApi.QryInvestorPosition();
         }
     }
 }
---
+++ autotrade/strategy/MAReverseStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CTPMdApi;
+
+namespace autotrade.strategy
+{
+    class MAReverseStrategy
+    {
+        private int days;
+        private String instrumentId;
+        private MA ma;
+        private double maPrice = 0;
+        public MAReverseStrategy(int days, string instrumentId)
+        {
+            this.days = days;
+            this.instrumentId = instrumentId;
+
+            ma = new MA(instrumentId, days);
+            maPrice = ma.GetMA();
+        }
+
+        public bool Match(CThostFtdcDepthMarketDataField pDepthMarketData)
+        {
+            double lastPrice = pDepthMarketData.LastPrice;
+            if (lastPrice > maPrice*1.02) return true;
+            else if (lastPrice < maPrice*0.98) return true;
+            else return false;
+        }
+    }
+}
