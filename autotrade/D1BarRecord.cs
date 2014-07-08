def55ad1d3b231978a3c3285e7abee224d08c9c6
stockcode <stockcode@sina.cn>
2014-07-08 17:29:59 +0800

    combo

---
+++ autotrade/D1BarRecord.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace autotrade
+{
+    class D1BarRecord
+    {
+
+        public ObjectId Id
+        {
+            get;
+            set;
+        }
+
+        public string _class { get; set; }
+
+        public string date
+        {
+            get;
+            set;
+        }
+
+        public double open
+        {
+            get;
+            set;
+        }
+
+        public double high
+        {
+            get;
+            set;
+        }
+
+        public double low
+        {
+            get;
+            set;
+        }
+
+        public double close
+        {
+            get;
+            set;
+        }
+
+        public double amount
+        {
+            get;
+            set;
+        }
+
+        public double volume
+        {
+            get;
+            set;
+        }        
+        
+    }
+}
---
+++ autotrade/MA.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace autotrade
+{
+    internal class MA
+    {
+        private readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private int days;
+        private String instrumentId;
+        private MongoCollection<D1BarRecord> collection;
+
+        public MA(string instrumentId, int days)
+        {
+            this.instrumentId = instrumentId;
+            this.days = days;
+
+            InitDb();
+        }
+
+        private void InitDb()
+        {
+            string connectionString = "mongodb://115.28.160.121";
+            var client = new MongoClient(connectionString);
+            MongoServer server = client.GetServer();
+            MongoDatabase database = server.GetDatabase("future");
+            collection = database.GetCollection<D1BarRecord>("d1BarRecord");
+
+        }
+
+        public double GetMA()
+        {
+            IQueryable<D1BarRecord> query =
+                (from e in collection.AsQueryable()
+                 orderby e.date descending
+                 select e).Take(days + 1).Skip(1);
+
+            double avg = 0;
+
+            foreach (D1BarRecord record in query)
+            {                
+                avg += record.close;
+            }
+
+            avg = avg / days;
+
+            avg = double.Parse(avg.ToString("F1"));
+
+            return avg;
+        }
+    }
+}
